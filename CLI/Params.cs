4637a697c1284225e3fd156cf4e2bd6242d3c42f
Le0Michine <leomichine@gmail.com>
2017-04-26 16:37:24 +0200

    Update description

--- CLI/Params.cs
+++ CLI/Params.cs
@@ -2,20 +2,19 @@
 
 namespace CLI
 {
-    [ParsableClass("Example CLI Parsable", "This is a description.", FooterText = "This is the footer text.")]
+    [ParsableClass("DNN zip packager", "Creates zip packages for DNN modules using provided config files.", FooterText = "More detailed information available on the github page\nhttps://github.com/Le0Michine/dnn-zip-bundler-cs")]
     public class Params : Parsable
     {
-        [ParsableArgument("config", ShortName = 'c', ImpliedPosition = 1, Required = true,
-            DefaultValue = "bundle.config.json", Example = "-c 'path_to_config.json'")]
+        [ParsableArgument("config", ShortName = 'c', ImpliedPosition = 1, Required = true, Description = "path to json config file")]
         public string Config { get; set; }
 
-        [ParsableArgument("bumpBuild", ShortName = 'b', DefaultValue = false)]
+        [ParsableArgument("bumpBuild", ShortName = 'b', DefaultValue = false, Description = "increment build number")]
         public bool BumpBuild { get; set; }
 
-        [ParsableArgument("bumpSprint", ShortName = 's', DefaultValue = false)]
+        [ParsableArgument("bumpSprint", ShortName = 's', DefaultValue = false, Description = "increment sprint number and reset build number to 1")]
         public bool BumpSprint { get; set; }
 
-        [ParsableArgument("targetVersion", ShortName = 'v')]
+        [ParsableArgument("targetVersion", ShortName = 'v', Description = "set specific version")]
         public string TargetVersion { get; set; }
     }
 }
